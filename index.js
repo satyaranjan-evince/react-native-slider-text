d41e14815a25b87079f5402abaf0c95e3106fa87
satyaranjan-evince <73092761+satyaranjan-evince@users.noreply.github.com>
2021-08-17 15:01:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import Slider from '@react-native-community/slider';
-const width = Dimensions.get('window').width;
+const width = Dimensions.get('window').width * .85;
 
 const SliderText = (props) => {
   const multiplier = props.multiplier || 1.15;
@@ -9,9 +9,10 @@ const SliderText = (props) => {
   const stepValue = props.stepValue || 1;
   const value = props.value || 0;
   const logic = maximumValue * multiplier;
-  const [ sliderValue, setSliderValue ] = useState(props.sliderValue || 0);
+  const [sliderValue, setSliderValue] = useState(props.sliderValue || 0);
 
   const left = sliderValue >= 100000000 ? sliderValue * width / logic - 40 : sliderValue * width / logic;
+  console.log("SLIDER VALAUE :::", props.sliderValue,left)
 
   const sendSliderValue = (slider) => {
     setSliderValue(slider);
@@ -22,26 +23,28 @@ const SliderText = (props) => {
     <View style={[styles.slider, props.containerStyle]}>
       <View
         style={{
-          transform: [{translateX: left}]
+          transform: [props.isRTL ?{ translateX: -left } : { translateX: left }]
         }}
       >
-        <Text style={[ styles.text, props.customCountStyle ]}>{Math.floor(sliderValue)}</Text>
+        {props.renderCustomLabel !== undefined ? props.renderCustomLabel() :
+          <Text style={[styles.text, props.customCountStyle]}>{Math.floor(sliderValue)}</Text>}
       </View>
       <Slider
-        style={[styles.slider, props.sliderStyle]}
+        style={[styles.slider, props.sliderStyle,props.isRTL ?{transform :[{rotateY : "180deg"}]}:{}]}
         minimumValue={props.minimumValue || 0}
         maximumValue={maximumValue}
         step={stepValue}
         minimumTrackTintColor={props.minimumTrackTintColor || '#000'}
         thumbTintColor={props.thumbTintColor || '#000'}
-        maximumTrackTintColor={props.maximumTrackTintColor || '#e3e3e3'}
+        maximumTrackTintColor={props.maximumTrackTintColor || '#999'}
         onValueChange={(e) => sendSliderValue(e)}
         onSlidingComplete={props.onSlidingComplete}
         value={value}
+          
       />
       <View style={styles.row}>
-        <Text style={[ styles.customLabel, props.customLabelStyle ]}>{props.minimumValueLabel || 'Start'}</Text>
-        <Text style={[ styles.customLabel, props.customLabelStyle ]}>{props.maximumValueLabel || 'End'}</Text>
+        <Text style={[styles.customLabel, props.customLabelStyle]}>{props.minimumValueLabel || ''}</Text>
+        <Text style={[styles.customLabel, props.customLabelStyle]}>{props.maximumValueLabel || ''}</Text>
       </View>
     </View>
   );
@@ -55,8 +58,9 @@ const styles = StyleSheet.create({
   },
   slider: {
     width: width - 20,
-    marginVertical: 20,
-    alignSelf: 'center'
+    marginTop: 10,
+    alignSelf: 'center',
+    marginBottom:-10
   },
   row: {
     flexDirection: 'row',
